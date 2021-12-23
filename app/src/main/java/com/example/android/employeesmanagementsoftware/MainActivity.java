f4d9119ec199816269eee6828599bbbc3b96fada
Tejbirsingh7878 <ks.singh7888@gmail.com>
2021-12-23 21:47:57 +0530

    Android project

---
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.employeesmanagementsoftware;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
+import com.example.android.employeesmanagementsoftware.DepartmentDB.DepartmentActivity;
+import com.example.android.employeesmanagementsoftware.DepartmentDB.MyDepartmentRecyclerViewAdapter;
+import com.example.android.employeesmanagementsoftware.data.Contracts.EmployeeContract;
+import com.example.android.employeesmanagementsoftware.data.DBHelpers.EmployeesManagementDbHelper;
+
+/*
+made by Tejbir singh and team
+ */
+// see how to connect 3 main activity ** department ** employies **tasks
+public class MainActivity extends AppCompatActivity {
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+    private static int SPLASH_TIME_OUT = 950;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent home = new Intent(MainActivity.this,StartingPageActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
---
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/StartingPageActivity.java
@@ -0,0 +1,138 @@
+package com.example.android.employeesmanagementsoftware;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+ import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.employeesmanagementsoftware.DepartmentDB.DepFragment;
+import com.example.android.employeesmanagementsoftware.TaskCreation.TaskCreation;
+import com.example.android.employeesmanagementsoftware.Utilities.SectionsPageAdapter;
+import com.example.android.employeesmanagementsoftware.data.DBHelpers.EmployeesManagementDbHelper;
+import com.example.android.employeesmanagementsoftware.taskDB.TasksFragment;
+
+
+public class StartingPageActivity extends AppCompatActivity {
+
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPageAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private FloatingActionButton fab;
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_page);
+        //Add ACTION BAR
+        toolbar =  findViewById(R.id.toolbar);
+        toolbar.setTitle("Department");
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the two
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager =  findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(0);
+        setupViewPager(mViewPager);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        fab =  findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DepartmentCreation.class);
+                intent.putExtra("IsEdit", false);
+                startActivity(intent);
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 1) {
+                    toolbar.setTitle("Tasks");
+                    setSupportActionBar(toolbar);
+                    fab = findViewById(R.id.fab);
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(getApplicationContext(), TaskCreation.class);
+                            intent.putExtra("task_id",(long)-1);
+                            startActivity(intent);
+                        }
+                    });
+                } else {
+                    toolbar.setTitle("Departments");
+                    setSupportActionBar(toolbar);
+                    fab =  findViewById(R.id.fab);
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(getApplicationContext(), DepartmentCreation.class);
+                            intent.putExtra("IsEdit", false);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new DepFragment(), "DEPARTMENTS");
+        adapter.addFragment(TasksFragment.newInstance(), "TASKS");
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
+    }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_starting_page, container, false);
+        }
+    }
+
+}
